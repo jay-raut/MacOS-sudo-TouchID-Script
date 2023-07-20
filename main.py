9db261056abf53671f6a4e2fd0b9af86e95c1f8f
jay-raut <jayraut@my.yorku.ca>
2023-07-20 13:37:06 -0400

    Updated python script to make a backup of sudo file before making changes

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 #script must run as sudo
 import filecmp
 import shutil
+import subprocess
+
 sudo_file_location = "/etc/pam.d/sudo"
+sudo_backup_location = "/etc/pam.d/sudo-backup"
 replacement_sudo = "replacement_sudo_file"
 
 
@@ -11,7 +14,13 @@ def check_tid(): #check if sudo file has sufficient auth tid.so string
 
 
 def modify_sudo_file(): #modify sudo file with modified sudo file
+    create_sudo_backup()
     shutil.copyfile(replacement_sudo, sudo_file_location)
+
+def create_sudo_backup(): #makes a backup copy for sudo file
+    open(sudo_backup_location, 'a').close()
+    shutil.copyfile(sudo_file_location, sudo_backup_location)
+
 
 if __name__ == '__main__':
     if not check_tid():
